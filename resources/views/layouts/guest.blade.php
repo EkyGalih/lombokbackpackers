f6bb7916e2e2d4ae51b619ff14c694f2aee068c2
EkyGalih <ekkygalih8@gmail.com>
2025-08-01 10:04:10 +0800

    ENABLE::comment guest place id

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -84,7 +84,7 @@
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Merriweather:wght@400;700&display=swap"
         rel="stylesheet">
     <!-- Google tag (gtag.js) -->
-    {{-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-46ELV7K7Q9"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-46ELV7K7Q9"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -94,7 +94,7 @@
         gtag('js', new Date());
 
         gtag('config', 'G-46ELV7K7Q9');
-    </script> --}}
+    </script>
 </head>
 
 <body class="bg-white text-gray-800 text-sm antialiased font-sans">
